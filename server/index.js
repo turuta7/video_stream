ca48608360da95d22744fb885c501f0c1de0bf42
Aleksandr Turuta <turuta51@gmail.com>
2023-09-22 15:00:59 +0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path"); // Подключаем модуль path
+const app = express();
+const port = 3001;
+
+app.get("/video", (req, res) => {
+  //   const videoPath = "test.mp4";
+  const videoPath = path.join(__dirname, "test.mp4");
+  const stat = fs.statSync(videoPath);
+  const fileSize = stat.size;
+  const range = req.headers.range;
+
+  if (range) {
+    const parts = range.replace(/bytes=/, "").split("-");
+    const start = parseInt(parts[0], 10);
+    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+
+    const chunksize = end - start + 1;
+    const file = fs.createReadStream(videoPath, { start, end });
+    const head = {
+      "Content-Range": `bytes ${start}-${end}/${fileSize}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": chunksize,
+      "Content-Type": "video/mp4",
+    };
+
+    res.writeHead(206, head);
+    file.pipe(res);
+  } else {
+    const head = {
+      "Content-Length": fileSize,
+      "Content-Type": "video/mp4",
+    };
+    res.writeHead(200, head);
+    fs.createReadStream(videoPath).pipe(res);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
